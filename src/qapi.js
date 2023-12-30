23615c589a7dbe0ed5db0d4cb19cef5e165d2de6
AirLinkDev <91385331+AirLinkDev@users.noreply.github.com>
2023-12-29 20:02:13 -0800

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"658f7a141d712a00232d6486",
+				"658f7a141d712a00232d6489"
+			]
+		},
+		"658f7a141d712a00232d6486": {
+			"id": "658f7a141d712a00232d6486",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"658f7a141d712a00232d6489": {
+			"id": "658f7a141d712a00232d6489",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
